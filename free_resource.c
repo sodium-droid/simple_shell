d6d207ade22d8385feb889e2225282ebff64a12e
Azeez Adebayo <debaycisse@gmail.com>
2023-12-10 01:48:17 -0800

    add function description and betty compliance
    ensure betty compliance and add documentation
    or description for both function and its parameters

--- free_resource.c
+++ free_resource.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * free_res - frees the used resources, especially a character pointer
+ * @lineptr: the character pointer to be freed
+ * @n: this variable is reset back to zero
+ */
 void free_res(char **lineptr, size_t *n)
 {
 	free(*lineptr);
